3bd62fd5cead5a141103fdb7744a5b5b9a2c7d7e
Chris Rinehart <chris.m.rinehart@gmail.com>
2023-12-07 13:46:22 -0500

    court_date_checker.py
    BIG update, started using selenium and all of its directories and imports. Furthermore, modified all functions to include proper HTML and CSS source code. Also added in more error and exception handling and added comments for readability.

--- court_date_checker.py
+++ court_date_checker.py
@@ -1,7 +1,14 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-def submit_a_number(session, client_a_number):
+def submit_a_number(driver, client_a_number):
     url = 'https://acis.eoir.justice.gov/en/'
 
     # Ensure client_a_number is a string and has the correct length
@@ -9,67 +16,110 @@ def submit_a_number(session, client_a_number):
     if len(client_a_number) != 9:
         raise ValueError("A-Number must be 9 digits long")
 
-    # Splitting the A-Number into individual digits
-    digits = list(client_a_number)
+    try:
+        # Open the website
+        driver.get(url)
 
-    # Constructing data payload for the POST request
-    data = {}
-    for i, digit in enumerate(digits):
-        input_id = f"3e9af2d5-6fd2-44bf-9391-f86e83d609c4-{i}"
-        data[input_id] = digit
+        time.sleep(7)
 
-    # Sending a POST request to the URL with the constructed data payload
-    response = session.post(url, data=data)
-    return response
+        try:
+            accept_button = WebDriverWait(driver, 100).until(
+                EC.element_to_be_clickable((By.XPATH, "//button[contains(text(), 'I Accept')]"))
+            )
+            accept_button.click()
+        except Exception as e:
+            printf(f"An error occurred when trying to click the accept buitton: {e} ")
+            # If the pop-up doesn't exist or there's an error, continue
+            pass
 
+        digits = list(client_a_number)
 
+        # Find and interact with the input fields using WebDriverWait
+        for i, digit in enumerate(digits):
+            input_id = f"3e9af2d5-6fd2-44bf-9391-f86e83d609c4-{i}"
+            input_field = WebDriverWait(driver, 100).until(
+                EC.presence_of_element_located((By.ID, input_id))
+            )
+            input_field.send_keys(digit)
 
-def get_court_date_info(session):
-    url = 'https://acis.eoir.justice.gov/en/caseInformation'
-    response = session.get(url)
+        # Submit the form     
+        submit_button = WebDriverWait(driver, 100).until(
+            EC.element_to_be_clickable((By.ID, 'btn_submit'))
+        )
+        submit_button.click()
 
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
+        # Wait for the redirection to the new URL (adjust the timeout as needed)
+        WebDriverWait(driver, 50).until(
+            EC.url_to_be('https://acis.eoir.justice.gov/en/caseInformation')
+        )
 
-        # Searching for the first 'section' element in the HTML
-        # This is where the court information is assumed to be present
-        court_info_section = soup.find('section')
-        return court_info_section.get_text().strip() if court_info_section else "Court date not found"
-    else:
+        return driver
+
+    except Exception as e:
+        print(f"An error occurred during submission: {e}")
+        return None
+
+def get_court_date_info(driver):
+    try:
+        # Wait for the court information to load
+        # This locator assumes that 'Hearing' is a unique word preceding the date
+        hearing_info_element = WebDriverWait(driver, 100).until(
+            EC.presence_of_element_located((By.XPATH, "//strong[contains(text(), 'hearing')]"))
+        )
+
+        # Extract the text, which is expected to be the hearing date
+        # The following sibling should contain the date based on the structure seen in the screenshot
+        court_date_element = hearing_info_element.find_element_by_xpath('./following-sibling::strong')
+        court_date_text = court_date_element.text
+
+        return court_date_text.strip() if court_date_text else "Court date not found"
+
+    except Exception as e:
+        print(f"An error occurred while retrieving court information: {e}")
         return "Failed to retrieve data"
 
+
+
+
 def main():
-    # Initialize an empty list to store client "A-Numbers"
+
     clients = []
 
-    # Prompt the user to enter "A-Numbers" and quit when done
-    print('Please enter client "A-Numbers". Enter "quit" when done:')
+    print('Please enter client "A-Numbers"(no symbols, just nine digits). Enter "done" when done:')
     while True:
         a_number = input('Enter "A-Number": ')
-        if a_number.lower() == 'quit':
-            break  # Exit the loop if user enters 'quit'
+        if a_number.lower() == 'done':
+            break  # Exit the loop if the user enters 'quit'
         clients.append(a_number)  # Append the entered "A-Number" to the clients list
 
-    # Create a session object from requests library to persist session across HTTP requests
-    with requests.Session() as session:
+    chrome_options = Options()
+    user_agent = "Penelope Cerrundolo"
+    chrome_options.add_argument(f"user-agent={user_agent}")
+    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+    chrome_options.add_argument("--disable-infobars")
+    chrome_options.add_argument("--disable-extensions")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+
+    # Initialize the WebDriver with the Chrome options
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+
+
+    with driver:
         for client in clients:
             try:
                 # Attempt to submit the client's "A-Number" using the submit_a_number function
-                submit_response = submit_a_number(session, client)
+                driver = submit_a_number(driver, client)
 
-                # Check if the submission was successful (HTTP status code 200)
-                if submit_response.status_code == 200:
+                if driver:
+                    # At this point, you are on the new URL where the court date info is present
                     # Retrieve court date information using the get_court_date_info function
-                    result = get_court_date_info(session)
+                    result = get_court_date_info(driver)
                     print(f"Court date for {client}: {result}")
                 else:
-                    # If submission failed, print the status code for debugging
-                    print(f"Failed to submit data for {client}: {submit_response.status_code}")
-            except requests.exceptions.RequestException as e:
-                # Handle network-related exceptions such as connection errors or timeouts
-                print(f"Network error occurred: {e}")
+                    print(f"Failed to submit data for {client}")
+
             except Exception as e:
-                # Handle any other unexpected exceptions
                 print(f"An unexpected error occurred: {e}")
 
 if __name__ == "__main__":
